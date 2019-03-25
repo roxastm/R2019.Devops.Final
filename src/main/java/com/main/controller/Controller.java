11cbf476db5d94363d0bbe0a10cabe7c57c0ab7b
RACKY LAGUERTA <racky.laguerta@ibm.com>
2019-03-25 18:49:09 +0800

    initial commit

---
+++ src/main/java/com/main/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
